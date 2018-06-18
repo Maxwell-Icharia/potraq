034dbc6c9b35f55a88200c83ca87053381a5c6f1
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-06-19 00:43:31 +0300

    [Add] - Functionality - Client Side

--- potraq/src/java/models/User.java
+++ potraq/src/java/models/User.java
@@ -1,16 +1,33 @@
 package models;
 
+import database.DBConnect;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "User")
+@RequestScoped
+
 public class User {
     
     private int id_number;
     private String first_name;
     private String surname;
-    private char email;
+    private String email;
     private char phone_number;
     private String dob;
     private String gender;
     private String password;
-
+    private String confirm_password;
+    private String heading;
+    private String body;    
+    
     public int getId_number() {
         return id_number;
     }
@@ -35,11 +52,11 @@ public class User {
         this.surname = surname;
     }
 
-    public char getEmail() {
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(char email) {
+    public void setEmail(String email) {
         this.email = email;
     }
 
@@ -74,7 +91,93 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getConfirm_password() {
+        return confirm_password;
+    }
+
+    public void setConfirm_password(String confirm_password) {
+        this.confirm_password = confirm_password;
+    }
     
+    public static final Pattern VALID_EMAIL_REGEX = 
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
     
+    public static boolean validateEmail(String mail){
+        
+       Matcher matcher = VALID_EMAIL_REGEX.matcher(mail);
+       return matcher.find();
+        
+    }
+    
+    public void checkData(){
+        
+        
+        if(validateEmail(this.getEmail()) == false){
+            heading = "Email Invalid Format";
+            body = "Email Missing '@' Symbol";
+            FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_WARN, heading, body);
+            FacesContext.getCurrentInstance().addMessage(null, facesMsg);
+        }
+//        if(){
+//            
+//        }
+//        if(){
+//            
+//        }
+//        if(){
+//            
+//        }
+//        if(){
+//            
+//        }
+//        if(){
+//            
+//        }
+//        if(){
+//            
+//        }
+//        if(){
+//            
+//        }
+//        if(){
+//            
+//        }
+//        if(){
+//            
+//        }
+
+        FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_INFO, heading, body);
+        FacesContext.getCurrentInstance().addMessage(null, facesMsg);
+        
+    }
+    
+    public void checkLogin(){
+        
+        
+        
+    }
+    
+    public void saveUser() throws SQLException{
+     
+        Statement save;
+        
+        DBConnect con = new DBConnect();
+        
+        Connection conn = con.connect();
+        
+        save = conn.createStatement();
+        
+        String sql = "";
+        
+        save.execute(sql);
+        
+    }
+    
+    public void logUser(){
+        
+        
+        
+    }
     
 }
